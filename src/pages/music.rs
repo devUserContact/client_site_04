79d9663f5692e2f4a21e61f7c9fdd5eb475e6018
devUserContact <developer.user.contact@protonmail.com>
2024-05-09 09:11:42 -0700

    added `Project` struct

--- src/pages/music.rs
+++ src/pages/music.rs
@@ -1,11 +1,24 @@
-use leptos::*;
 use leptos::IntoView;
+use leptos::*;
 
 #[component]
 pub fn PageMusic() -> impl IntoView {
+    #[derive(Debug, Clone)]
+    struct Project {
+        title: String,
+        year_created: u16,
+        artwork_url: String,
+        spotify_url: String,
+        bandcamp_url: String,
+        description: String,
+    }
+
     view! {
         <div class="container">
             <p class="textStyle">music page</p>
+            <div class="musicGrid">
+
+            </div>
         </div>
     }
 }
