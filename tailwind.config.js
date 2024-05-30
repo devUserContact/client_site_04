b1f557cffba708e690b9d5c589d15833d7be66d0
devUserContact <developer.user.contact@protonmail.com>
2024-05-30 10:26:07 -0700

    fixed global font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Uncut-Sans-Regular'],
+    },
   },
   plugins: [],
 }
