dc1eb6aa7905724ebe08a01d6f95777279220bb5
devUserContact <developer.user.contact@protonmail.com>
2024-05-29 17:15:18 -0700

    added text to `About` page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,7 +2,14 @@ import type { Component } from 'solid-js'
 
 export default function About() {
   return (
-    <div>
+    <div class='grid grid-cols-1 mt-10 place-items-center'>
+      <div class='inline'>
+        <p class='inline'>
+        {`Crane Song is a alternative singer-songwriter project from artist and
+          musician `}
+        </p>
+        <p class='inline italic'>{`Maxwell` }</p>
+      </div>
     </div>
   )
 }
